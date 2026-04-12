c29044d9ab33487600af9a56e19d8d1a48763be2
Rushikesh <rginsane882@gmail.com>
2026-04-12 20:24:47 +0530

    Final portfolio UI update

---
+++ index.js
@@ -0,0 +1,8 @@
+function openModal(image){
+document.getElementById("certificateModal").style.display="block"
+document.getElementById("modalImage").src=image
+}
+
+function closeModal(){
+document.getElementById("certificateModal").style.display="none"
+}
